f766cf27b9ddedc03c38f309741f98f28110284f
KangMinGyo <kang15567@kakao.com>
2022-11-07 21:25:52 +0900

    프로젝트 생성

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by KangMingyo on 2022/11/07.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
