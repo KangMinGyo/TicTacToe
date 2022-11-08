c828fe7601f87ff9db4faabbefd3198b1d3015b7
KangMinGyo <kang15567@kakao.com>
2022-11-08 22:02:02 +0900

    feat: Alert 기능 추가

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     
     @State private var moves: [Move?] = Array(repeating: nil, count: 9)
     @State private var isGameboardDisabled = false
+    @State private var alertItem: AlertItem?
     
     var body: some View {
         GeometryReader { geometry in
@@ -35,21 +36,47 @@ struct ContentView: View {
                         .onTapGesture {
                             if isSquareOccupied(in: moves, forIndex: i) { return }
                             moves[i] = Move(player: .human, boardIndex: i)
-                            isGameboardDisabled = true
                             
                             //이겼는지 체크
+                            if checkWinCondition(for: .human, in: moves) {
+                                alertItem = AlertContext.humanWin
+                                return
+                            }
+                            
+                            if checkForDraw(in: moves) {
+                                alertItem = AlertContext.draw
+                                return
+                            }
+                            
+                            isGameboardDisabled = true
+                            
                             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                                 let computerPosition = determineComputerMovePosition(in: moves)
                                 moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
                                 isGameboardDisabled = false
+                                
+                                if checkWinCondition(for: .computer, in: moves) {
+                                    alertItem = AlertContext.computerWin
+                                    return
+                                }
+                                
+                                if checkForDraw(in: moves) {
+                                    alertItem = AlertContext.draw
+                                    return
+                                }
                             }
                         }
                     }
                 }
                 Spacer()
             }
-//            .disabled(isGameboardDisabled)
+            .disabled(isGameboardDisabled)
             .padding()
+            .alert(item: $alertItem, content: { alertItem in
+                Alert(title: alertItem.title,
+                      message: alertItem.message,
+                      dismissButton: .default(alertItem.buttonTitle, action: { resetGame() }))
+            })
         }
     }
     
@@ -58,6 +85,45 @@ struct ContentView: View {
     }
     
     func determineComputerMovePosition(in moves: [Move?]) -> Int {
+        
+        // AI가 이길수있으면 이기자
+        let winPatterns: Set<Set<Int>> = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        
+        let computerMoves = moves.compactMap { $0 }.filter { $0.player == .computer }
+        let computerPositions = Set(computerMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns {
+            let winPositions = pattern.subtracting(computerPositions)
+            
+            if winPositions.count == 1 {
+                let isAvaiable = !isSquareOccupied(in: moves, forIndex: winPositions.first!)
+                if isAvaiable { return winPositions.first! }
+            }
+        }
+        
+        
+        // AI가 이길수없으면 막자
+        let humanMoves = moves.compactMap { $0 }.filter { $0.player == .human }
+        let humanPositions = Set(humanMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns {
+            let winPositions = pattern.subtracting(humanPositions)
+            
+            if winPositions.count == 1 {
+                let isAvaiable = !isSquareOccupied(in: moves, forIndex: winPositions.first!)
+                if isAvaiable { return winPositions.first! }
+            }
+        }
+        
+        
+        // 가운데 블럭이 비어있으면 가운데 블럭 선택하자
+        let centerSquare = 4
+        if !isSquareOccupied(in: moves, forIndex: 4) {
+            return centerSquare
+        }
+        
+        
+        // AI가 가운데를 먹지못했다면 랜덤으로 입력하자
         var movePosition = Int.random(in: 0..<9)
         
         while isSquareOccupied(in: moves, forIndex: movePosition) {
@@ -67,6 +133,24 @@ struct ContentView: View {
         return movePosition
     }
     
+    func checkWinCondition(for player: Player, in movies: [Move?]) -> Bool {
+        let winPatterns: Set<Set<Int>> = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        
+        let playerMoves = moves.compactMap { $0 }.filter { $0.player == player }
+        let playerPositions = Set(playerMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns where pattern.isSubset(of: playerPositions) { return true }
+
+        return false
+    }
+    
+    func checkForDraw(in moves: [Move?]) -> Bool {
+        return moves.compactMap { $0 }.count == 9
+    }
+    
+    func resetGame() {
+        moves = Array(repeating: nil, count: 9)
+    }
 }
 
 enum Player {
